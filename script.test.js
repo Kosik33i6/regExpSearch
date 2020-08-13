9441095cc73f5b891b6e4e4f547016bb42249244
Krzysztof Kos <krzysztof.kos33@gmail.com>
2020-08-13 10:54:37 +0200

    add tests

--- script.test.js
+++ script.test.js
@@ -4,4 +4,17 @@ describe('regexHashtag', () => {
   test('should match the expected hashtag', () => {
     expect('#code').toMatch(regexHashtag)
   })
-})
+});
+
+describe('Regexp names', () => {
+  test('Should match name', () => {
+    expect('Sylwia lives in Wrocław').toMatch(regexNames);
+  });
+  test('Should be a null when string is empty', () => {
+    expect(''.match(regexNames)).toBeNull();
+  });
+  test('Should not find a name with lowercase', () => {
+    const name = "adam";
+    expect(name.match(regexNames)).toBeFalsy();
+  });
+});
