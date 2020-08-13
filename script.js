dcdd88c99a74c23430b87badf5b10e01861656d0
Krzysztof Kos <krzysztof.kos33@gmail.com>
2020-08-13 15:02:13 +0200

    update test and regexp

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const regexHashtag = /#[a-z0-9_]+/gm,
-      regexNames = /\b([A-ZŁŻ][a-zł]+)($|(?= ))/g,
-      regexSpecialChar = /(^|(?<=\s))[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]($|(?=\s))/g
+  regexNames = /(^|(?<=\s))([A-ZŁŻ][a-zł]+)($|(?=\s))/g,
+  regexSpecialChar = /(^|(?<=\s))[-!$%^&*()_+|~=`{}[:;<>?,.@#\]]($|(?=\s))/g
 
 module.exports = {
   regexHashtag,
